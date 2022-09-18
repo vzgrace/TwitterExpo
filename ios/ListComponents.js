abd7abe8dec4607506961971a40dda17544dc4a4
bswain12 <bswain@bu.edu>
2022-09-17 21:00:44 -0400

    I created a Button file and a listcomponent file which im currently using for testing. Also a notes page where im making general notes of react and the Twitter API

---
+++ ios/ListComponents.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ListComponents = (props) => {
+    
+    return (
+            
+        <div>
+            
+            <h1>{props.tex}</h1>
+            
+        </div>
+    );
+    
+};
+
+export {ListComponents};
